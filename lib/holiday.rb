34cd0cfd9863425cb5295c09f8dbf33c9707a276
Kaushal Patel <kaushalpatel778@gmail.com>
2018-03-25 20:45:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
+  # code here NOT DONE
   # remember to return the updated hash
 #  holiday_hash[:season][:holiday_name] = supply_array
  holiday_hash[:fall][:columbus_day] =  ["Flags", "Parade Floats", "Italian Food"]
